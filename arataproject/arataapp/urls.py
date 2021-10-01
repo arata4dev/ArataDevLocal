39f8632d5df61ba69b3a7079afd5a6ff1de8d1d9
Arata <arata.gg1e@googlemail.com>
2021-10-01 20:30:25 +0800

    master

--- arataproject/arataapp/urls.py
+++ arataproject/arataapp/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.Index.as_view(), name="index"),
     path('about/', AboutView.as_view()),
     path('detail/<pk>', views.Detail.as_view(), name="detail"),
+    path('create/', views.Create.as_view(), name="create"),
+    path('update/<pk>', views.Update.as_view(), name="update"),
+    path('delte/<pk>', views.Delete.as_view(), name="delete"),
 ]
 
 # pk = primary key (IDのこと)
--- arataproject/arataapp/views.py
+++ arataproject/arataapp/views.py
@@ -3,17 +3,53 @@ from django.views.generic import TemplateView
 class AboutView(TemplateView):
     template_name = "about.html"
 
-
-###
-
-from django.views.generic import ListView, DetailView
 from. models import Post
 
-# 簡単に一覧を作るためのView
-# ListViewは自動的に post_list.html というファイルを探してくれる
+
+
+### 一覧
+### テンプレート名は post_list.html にする必要
+
+from django.views.generic import ListView
+
 class Index(ListView):
     model = Post
 
-# 簡単に詳細ページを作るためのView
+
+### 個別ページ
+### テンプレート名は post_detail.html にする必要
+
+from django.views.generic import DetailView
+
 class Detail(DetailView):
     model = Post
+
+
+### 新規作成
+### テンプレート名は post_form.html にする必要
+
+from django.views.generic.edit import CreateView
+
+class Create(CreateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+
+
+### 編集
+
+from django.views.generic.edit import UpdateView
+
+class Update(UpdateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+
+
+### 削除
+### テンプレート名は post_confirm_delete にする必要
+
+from django.views.generic.edit import DeleteView
+
+class Delete(DeleteView):
+    model = Post
+    # 削除後に移動するURL
+    success_url = "/"
